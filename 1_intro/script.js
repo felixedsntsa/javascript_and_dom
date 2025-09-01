81129be22f852aec48bb04e0da0f4975756e7d77
Felix Edna Santosa <felixsantosa123@gmail.com>
2025-09-01 10:28:45 +0700

    intro to javascripts and dom

---
+++ 1_intro/script.js
@@ -0,0 +1,19 @@
+// DOM (Document Object Model)
+// DOM adalah antarmuka pemrograman untuk HTML yang merepresentasikan halaman web, sehingga program dapat mengubah dan memanipulasi strukturnya -mozilla
+// document => isi dari website
+// object => sebuah nilai atau tipe data dalam javascript (mempunyai banyak nilai, ada properti dan method)
+// model => representasi terhadap sesuatu
+// sehingga dom adalah representasi elemen HTML pada dokumen menjadi sebuah object
+
+// DOM Tree
+// html => head and body
+// head => title, meta, meta
+// body => h1, p, ul
+// p => a
+// ul => li
+
+// DOM
+// - antar =muka pemrograman berbasis objek yang merepresentasikan dokumen web
+// -  DOM membuat seluruh koponen dari halaman web dapat diakses dan dimanipulasi
+// - komponen => elemen html, atribut, text, dll
+// - DOM dapat dimanipulasi (dibuat, diubah, dihapus) menggunakan javascripts
