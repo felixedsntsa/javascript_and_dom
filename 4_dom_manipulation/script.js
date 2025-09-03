81133e865c2d65b35ef4f26b39bea20a235f3c59
Felix Edna Santosa <felixsantosa123@gmail.com>
2025-09-03 11:57:56 +0700

    dom manipulation #2

--- 4_dom_manipulation/script.js
+++ 4_dom_manipulation/script.js
@@ -28,7 +28,7 @@
 // a.setAttribute('id', 'link');
 
 // element.classList
-const p2 = document.querySelector('.p2');
+// const p2 = document.querySelector('.p2');
 // p2.classList.add('label'); -> menambah nama pada class yang dituju
 // p2.classList.remove('p2'); -> menghapus nama isi class
 // p2.classList.toggle('label'); -> kalau true tambahkan ke class nya
@@ -40,3 +40,58 @@ const p2 = document.querySelector('.p2');
 // p2.classList.contains('tiga'); -> jika ada nilai 'tiga' pada class p2 maka true, jika tidak false
 // p2.classList.contains('empat');
 // p2.classList.replace('tiga', 'empat'); -> mengubah nilai 'tiga' menjadi 'empat' di class p2
+
+
+
+// NODE MANIPULATION
+// document.createElement()
+// document.createTextNOde()
+// node.appendChild()
+// node.insertBefore()
+// parentNode.removeChild()
+// parentNode.replaceChild()
+
+// buat elemen baru
+const pBaru = document.createElement('p'); // document.createElement()
+const textPBaru = document.createTextNode('Paragraf baru'); // document.createTextNOde()
+// simpan tulisan ke dalam paragraf
+pBaru.appendChild(textPBaru); // node.appendChild()
+
+// simpan pBaru di akhir section A
+const sectionA = document.getElementById('a');
+sectionA.appendChild(pBaru);
+
+const liBaru = document.createElement('li');
+const teksLiBaru = document.createTextNode('item baru');
+liBaru.appendChild(teksLiBaru);
+
+const ul = document.querySelector('#b ul');
+const li2 = ul.querySelector('li:nth-child(2)');
+
+ul.insertBefore(liBaru, li2); // node.insertBefore() untuk parameternya yang pertama ambil yang mau ditambahkan lalu parameter kedua diletakkan sebelum elemen apa
+
+// parentNode.removeChild()
+const link = document.getElementsByTagName('a')[0];
+sectionA.removeChild(link);
+
+// parentNode.replaceChild()
+const sectionB = document.getElementById('b');
+const p4 = sectionB.querySelector('p');
+
+const h2Baru = document.createElement('h2');
+const teksH2Baru = document.createTextNode('Judul Baru!');
+h2Baru.appendChild(teksH2Baru);
+
+sectionB.replaceChild(h2Baru, p4);
+
+pBaru.style.backgroundColor = 'lightGreen'
+liBaru.style.backgroundColor = 'lightGreen'
+h2Baru.style.backgroundColor = 'lightGreen'
+
+// NODE MANIPULATION (NEW METHOD)
+// parentNode.append()
+// parentNOde.prepend()
+// childNode.before()
+// childNode.after()
+// childNode.remove()
+// childNode.replaceWith()
